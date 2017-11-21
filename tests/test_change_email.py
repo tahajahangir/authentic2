tahajahangir/authentic2

commit 16afddc6b9a4706e3181130492773baf07b40219

views: enforce A2_EMAIL_IS_UNIQUE on email change (fixes #19712)

src/authentic2/views.py
@@ -178,7 +178,14 @@ class EmailChangeView(cbv.TemplateNamesMixin, FormView):
             'link': link,
             'domain': request.get_host(),
             'token_lifetime': utils.human_duration(app_settings.A2_EMAIL_CHANGE_TOKEN_LIFETIME),
+            'password_reset_url': request.build_absolute_uri(reverse('password_reset')),
         }
+        qs = compat.get_user_model().objects.all()
+        if app_settings.A2_EMAIL_IS_UNIQUE:
+            ctx['email_is_not_unique'] = qs.filter(email=email).exclude(pk=user.pk).exists()
+        elif user.ou and user.ou.email_is_unique:
+            ctx['email_is_not_unique'] = qs.filter(email=email,
+                                                   ou=user.ou).exclude(pk=user.pk).exists()
 
         utils.send_templated_mail(
             email,
@@ -213,6 +220,14 @@ class EmailChangeVerifyView(TemplateView):
                 user_pk = token['user_pk']
                 email = token['email']
                 user = User.objects.get(pk=user_pk)
+                non_unique = False
+                if app_settings.A2_EMAIL_IS_UNIQUE:
+                    non_unique = User.objects.filter(email=email).exclude(pk=user_pk).exists()
+                elif user.ou and user.ou.email_is_unique:
+                    non_unique = User.objects.filter(email=email, ou=user.ou).exclude(
+                        pk=user_pk).exists()
+                if non_unique:
+                    raise forms.ValidationError(_('This email is already used by another account.'))
                 old_email = user.email
                 user.email = email
                 user.save()
@@ -234,6 +249,8 @@ class EmailChangeVerifyView(TemplateView):
             except User.DoesNotExist:
                 messages.error(request, _('your request for changing your email is for '
                     'an unknown user, try again'))
+            except forms.ValidationError as e:
+                messages.error(request, e.message)
             else:
                 return shortcuts.redirect('account_management')
         return shortcuts.redirect('email-change')

tests/test_change_email.py
@@ -1,0 +1,59 @@
+import re
+
+import utils
+
+
+def change_email(app, user, email, mailoutbox):
+    utils.login(app, user)
+    l = len(mailoutbox)
+    response = app.get('/accounts/change-email/')
+    response.form.set('email', email)
+    response.form.set('password', user.username)
+    response = response.form.submit()
+    assert len(mailoutbox) == l + 1
+    return mailoutbox[-1]
+
+
+def test_change_email(app, simple_user, user_ou1, mailoutbox):
+    email = change_email(app, simple_user, user_ou1.email, mailoutbox)
+    links = re.findall('https?://[^ \n]*', email.body)
+    assert links
+    link = links[0]
+    app.get(link)
+    simple_user.refresh_from_db()
+    # ok it worked
+    assert simple_user.email == user_ou1.email
+
+
+def test_change_email_email_is_unique(app, settings, simple_user, user_ou1, mailoutbox):
+    settings.A2_EMAIL_IS_UNIQUE = True
+    email = change_email(app, simple_user, user_ou1.email, mailoutbox)
+    links = re.findall('https?://[^ \n]*', email.body)
+    assert links
+    link = links[0]
+    # email change is impossible as email is already taken
+    assert 'password/reset' in link
+
+
+def test_change_email_ou_email_is_unique(app, simple_user, user_ou1, user_ou2, mailoutbox):
+    user_ou1.ou.email_is_unique = True
+    user_ou1.ou.save()
+    user_ou2.email = 'john.doe-ou2@example.net'
+    user_ou2.save()
+    email = change_email(app, simple_user, user_ou2.email, mailoutbox)
+    links = re.findall('https?://[^ \n]*', email.body)
+    assert links
+    link = links[0]
+    app.get(link)
+    simple_user.refresh_from_db()
+    # ok it worked for a differnt ou
+    assert simple_user.email == user_ou2.email
+    # now set simple_user in same ou as user_ou1
+    simple_user.ou = user_ou1.ou
+    simple_user.save()
+    email = change_email(app, simple_user, user_ou1.email, mailoutbox)
+    links = re.findall('https?://[^ \n]*', email.body)
+    assert links
+    link = links[0]
+    # email change is impossible as email is already taken in the same ou
+    assert 'password/reset' in link

commit 39c8cbb98979f7d0ec99bc62229244b01034802a

views: fix bug introduced in EmailChangeVerifyView by 16afddc6b9a47 (fixes #20186)
Ref #19712

src/authentic2/views.py
@@ -19,6 +19,7 @@ from django.contrib.auth import SESSION_KEY
 from django import http, shortcuts
 from django.core import mail, signing
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 from django.contrib import messages
 from django.utils.translation import ugettext as _
 from django.utils.http import urlencode, same_origin
@@ -210,6 +211,7 @@ class EmailChangeView(cbv.TemplateNamesMixin, FormView):
 email_change = decorators.setting_enabled('A2_PROFILE_CAN_CHANGE_EMAIL')(
     login_required(EmailChangeView.as_view()))
 
+
 class EmailChangeVerifyView(TemplateView):
     def get(self, request, *args, **kwargs):
         if 'token' in request.GET:
@@ -227,7 +229,7 @@ class EmailChangeVerifyView(TemplateView):
                     non_unique = User.objects.filter(email=email, ou=user.ou).exclude(
                         pk=user_pk).exists()
                 if non_unique:
-                    raise forms.ValidationError(_('This email is already used by another account.'))
+                    raise ValidationError(_('This email is already used by another account.'))
                 old_email = user.email
                 user.email = email
                 user.save()
@@ -249,7 +251,7 @@ class EmailChangeVerifyView(TemplateView):
             except User.DoesNotExist:
                 messages.error(request, _('your request for changing your email is for '
                     'an unknown user, try again'))
-            except forms.ValidationError as e:
+            except ValidationError as e:
                 messages.error(request, e.message)
             else:
                 return shortcuts.redirect('account_management')

tests/test_change_email.py
@@ -47,3 +47,17 @@ def test_change_email_ou_email_is_unique(app, simple_user, user_ou1, user_ou2, m
     link = utils.get_link_from_mail(email)
     # email change is impossible as email is already taken in the same ou
     assert 'password/reset' in link
+
+
+def test_change_email_is_unique_after_first_view(app, settings, simple_user, user_ou1, mailoutbox):
+    settings.A2_EMAIL_IS_UNIQUE = True
+    new_email = 'wtf@example.net'
+    email = change_email(app, simple_user, new_email, mailoutbox)
+    link = utils.get_link_from_mail(email)
+    # user_ou1 take the new email in the meantime
+    user_ou1.email = new_email
+    user_ou1.save()
+    # email change is impossible as email is already taken
+    link = utils.get_link_from_mail(email)
+    response = app.get(link).follow()
+    assert 'is already used by another account' in response.content

tests/test_views.py
@@ -2,7 +2,6 @@ from utils import login
 import pytest
 
 from django.core.urlresolvers import reverse
-from django.core import mail
 
 from authentic2.custom_user.models import User
 
@@ -13,18 +12,6 @@ def test_profile(app, simple_user):
     page = login(app, simple_user, path=reverse('account_management'))
     assert simple_user.first_name in page
     assert simple_user.last_name in page
-
-
-def test_email_change(app, simple_user):
-    page = login(app, simple_user, path=reverse('email-change'))
-    page = page.form.submit('cancel').follow()
-
-    page = app.get(reverse('email-change'))
-    page.form.set('email', 'john.doe2@example.net')
-    page.form.set('password', simple_user.username)
-    page = page.form.submit('Validate').follow()
-    assert len(mail.outbox) == 1
-    assert 'for 2 hours.' in mail.outbox[0].body
 
 
 def test_password_change(app, simple_user):