645f5b4d5424abe8461584f44bf98c68ba3aac8a
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2017-12-06 11:02:14 +0100

    tests: move test on password-reset functionality (#20455)

---
+++ tests/test_password_reset.py
@@ -0,0 +1,43 @@
+from django.core.urlresolvers import reverse
+
+import utils
+
+
+def test_send_password_reset_email(app, simple_user, mailoutbox):
+    from authentic2.utils import send_password_reset_mail
+    assert len(mailoutbox) == 0
+    send_password_reset_mail(
+        simple_user,
+        legacy_subject_templates=['registration/password_reset_subject.txt'],
+        legacy_body_templates=['registration/password_reset_email.html'],
+        context={
+            'base_url': 'http://testserver',
+        })
+    assert len(mailoutbox) == 1
+    url = utils.get_link_from_mail(mailoutbox[0])
+    relative_url = url.split('testserver')[1]
+    resp = app.get(relative_url, status=200)
+    resp.form.set('new_password1', '1234==aA')
+    resp.form.set('new_password2', '1234==aA')
+    resp = resp.form.submit().follow()
+    assert str(app.session['_auth_user_id']) == str(simple_user.pk)
+
+
+def test_view(app, simple_user, mailoutbox):
+    url = reverse('password_reset') + '?next=/moncul/'
+    resp = app.get(url, status=200)
+    resp.form.set('email', simple_user.email)
+    assert len(mailoutbox) == 0
+    resp = resp.form.submit()
+    assert resp['Location'].endswith('/moncul/')
+    assert len(mailoutbox) == 1
+    url = utils.get_link_from_mail(mailoutbox[0])
+    relative_url = url.split('testserver')[1]
+    resp = app.get(relative_url, status=200)
+    resp.form.set('new_password1', '1234==aA')
+    resp.form.set('new_password2', '1234==aA')
+    resp = resp.form.submit()
+    # verify user is logged
+    assert str(app.session['_auth_user_id']) == str(simple_user.pk)
+    # verify next_url was kept
+    assert resp['Location'].endswith('/moncul/')
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,52 +1,12 @@
 import pytest
 
-from django.core import mail
 from django.core.urlresolvers import reverse
 
-from authentic2.models import Attribute, AttributeValue
+from authentic2.models import Attribute
 
 import utils
 
 pytestmark = pytest.mark.django_db
-
-
-def test_send_password_reset_email(app, simple_user):
-    from authentic2.utils import send_password_reset_mail
-    assert len(mail.outbox) == 0
-    send_password_reset_mail(simple_user,
-                            legacy_subject_templates=['registration/password_reset_subject.txt'],
-                            legacy_body_templates=['registration/password_reset_email.html'],
-                            context={
-                               'base_url': 'http://testserver',
-                            })
-    assert len(mail.outbox) == 1
-    url = utils.get_link_from_mail(mail.outbox[0])
-    relative_url = url.split('testserver')[1]
-    resp = app.get(relative_url, status=200)
-    resp.form.set('new_password1', '1234==aA')
-    resp.form.set('new_password2', '1234==aA')
-    resp = resp.form.submit().follow()
-    assert str(app.session['_auth_user_id']) == str(simple_user.pk)
-
-
-def test_password_reset_view(app, simple_user):
-    url = reverse('password_reset') + '?next=/moncul/'
-    resp = app.get(url, status=200)
-    resp.form.set('email', simple_user.email)
-    assert len(mail.outbox) == 0
-    resp = resp.form.submit()
-    assert resp['Location'].endswith('/moncul/')
-    assert len(mail.outbox) == 1
-    url = utils.get_link_from_mail(mail.outbox[0])
-    relative_url = url.split('testserver')[1]
-    resp = app.get(relative_url, status=200)
-    resp.form.set('new_password1', '1234==aA')
-    resp.form.set('new_password2', '1234==aA')
-    resp = resp.form.submit()
-    # verify user is logged
-    assert str(app.session['_auth_user_id']) == str(simple_user.pk)
-    # verify next_url was kept
-    assert resp['Location'].endswith('/moncul/')
 
 
 def test_account_edit_view(app, simple_user):
@@ -55,7 +15,7 @@ def test_account_edit_view(app, simple_user):
     resp = app.get(url, status=200)
 
     attribute = Attribute.objects.create(name='phone', label='phone',
-            kind='string', user_visible=True, user_editable=True)
+        kind='string', user_visible=True, user_editable=True)
     resp = app.get(url, status=200)
     resp.form.set('edit-profile-phone', '0123456789')
     resp = resp.form.submit().follow()
