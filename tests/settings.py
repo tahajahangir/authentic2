65769ac3e8e88c14e51319a034d4dc0d6b9ac258
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2018-02-28 15:16:47 +0100

    tests: change default password to accelerate tests (fixes #22199)

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,10 @@
 import os
 
+# use a faster hasing scheme for passwords
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
+]
+
 LANGUAGE_CODE = 'en'
 DATABASES = {
     'default': {
