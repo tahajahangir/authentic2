f6ff455fdfa89a3e49e718123809c8add04b3d07
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2018-03-01 15:43:10 +0100

    tests: do not clobber all hashing algorithms (fixes #22199)

--- tests/settings.py
+++ tests/settings.py
@@ -1,9 +1,9 @@
 import os
 
+from authentic2.settings import PASSWORD_HASHERS
+
 # use a faster hasing scheme for passwords
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
-]
+PASSWORD_HASHERS = ('django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',) + PASSWORD_HASHERS
 
 LANGUAGE_CODE = 'en'
 DATABASES = {
