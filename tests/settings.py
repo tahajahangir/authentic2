f52c7445e604a7bbd3c730026f49ead017e6fdf6
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2018-03-01 19:14:22 +0100

    tests: fix PASSWORD_HASHERS setting to work with RBAC tests (fixes #22199)

--- tests/settings.py
+++ tests/settings.py
@@ -1,8 +1,9 @@
 import os
 
-from authentic2.settings import PASSWORD_HASHERS
-
 # use a faster hasing scheme for passwords
+if 'PASSWORD_HASHERS' not in locals():
+    from django.conf.global_settings import PASSWORD_HASHERS
+
 PASSWORD_HASHERS = ('django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',) + PASSWORD_HASHERS
 
 LANGUAGE_CODE = 'en'
